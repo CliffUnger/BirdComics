64edfbe49007220c4cd00f6fc26f42a38055ff01
CliffUnger <adrianobuccino@gmail.com>
2024-06-16 16:59:47 +0200

    servlet per aggiornare il numero di prodotti nel carrello

---
+++ src/main/java/com/birdcomics/controller/UpdateToCart.java
@@ -0,0 +1,124 @@
+package com.birdcomics.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.birdcomics.model.*;
+
+/**
+ * Servlet implementation class UpdateToCart
+ */
+@WebServlet("/UpdateToCart")
+public class UpdateToCart extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public UpdateToCart() {
+		super();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String userName = (String) session.getAttribute("username");
+		String password = (String) session.getAttribute("password");
+
+		if (userName == null || password == null) {
+
+			response.sendRedirect("login.jsp?message=Session Expired, Login Again!!");
+		}
+
+		// login Check Successfull
+
+		String userId = userName;
+		String prodId = request.getParameter("pid");
+		int pQty = Integer.parseInt(request.getParameter("pqty"));
+
+		CartServiceDAO cart = new CartServiceDAO();
+
+		ProductServiceDAO productDao = new ProductServiceDAO();
+
+		ProductBean product = null;
+		try {
+			product = productDao.getProductDetails(prodId);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		int availableQty = product.getProdQuantity();
+
+		PrintWriter pw = response.getWriter();
+
+		response.setContentType("text/html");
+
+		if (availableQty < pQty) {
+
+			String status;
+			try {
+				status = cart.updateProductToCart(userId, prodId, availableQty);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			status = "Only " + availableQty + " no of " + product.getProdName()
+					+ " are available in the shop! So we are adding only " + availableQty + " products into Your Cart"
+					+ "";
+
+			DemandBean demandBean = new DemandBean(userName, product.getProdId(), pQty - availableQty);
+
+			DemandServiceDAO demand = new DemandServiceDAO();
+
+			boolean flag = false;
+			try {
+				flag = demand.addProduct(demandBean);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			if (flag)
+				status += "<br/>Later, We Will Mail You when " + product.getProdName()
+						+ " will be available into the Store!";
+
+			RequestDispatcher rd = request.getRequestDispatcher("cartDetails.jsp");
+
+			rd.include(request, response);
+
+			pw.println("<script>document.getElementById('message').innerHTML='" + status + "'</script>");
+
+		} else {
+			String status = null;
+			try {
+				status = cart.updateProductToCart(userId, prodId, pQty);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			RequestDispatcher rd = request.getRequestDispatcher("cartDetails.jsp");
+
+			rd.include(request, response);
+
+			pw.println("<script>document.getElementById('message').innerHTML='" + status + "'</script>");
+		}
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
